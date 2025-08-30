2618f2f85e243018d0590168f9e52f40c82249fc
dingbat32 <sven.willett@gmail.com>
2025-08-30 22:11:07 +1000

    Fix CrowdSourcedData

--- backend/code/api/admin.py
+++ backend/code/api/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import TripInfo, Train, CarbonFootprint, CrowdsourcedData
+from .models import TripInfo, Train, CarbonFootprint, CrowdSourcedData
 
 admin.site.register(TripInfo)
 admin.site.register(Train)
 admin.site.register(CarbonFootprint)
-admin.site.register(CrowdsourcedData)
+admin.site.register(CrowdSourcedData)
