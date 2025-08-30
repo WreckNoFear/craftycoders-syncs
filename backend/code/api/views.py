91edb400fc964518b12d28dd97508e7bdef64c7b
Anthony <anthony.nguyen6002@gmail.com>
2025-08-30 23:57:21 +1000

    added views for carbonfootprint

--- backend/code/api/views.py
+++ backend/code/api/views.py
@@ -45,3 +45,21 @@ def request_trips(request):
         journey = tnsw.get_trip(start, end, API_KEY, 5)
         out = json.loads(journey)
         return JsonResponse(out)
+
+@csrf_exempt
+def CarbonFootprint(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        trip = data.get("trip")
+        transport_type = data.get("trip.origin_transport_type")
+        distance_km = data.get("distance_km")
+        if transport_type == "train":
+            carbon_emissions_transport_kg = distance_km * 0.04  # 0.04 kg CO2 per km for trains
+        elif transport_type == "metro":
+            carbon_emissions_transport_kg = distance_km * 0.00  # 0.00 kg CO2 per km for metro
+        else:
+            carbon_emissions_transport_kg = distance_km * 0.1   # Default value
+        carbon_emissions_car_kg = distance_km * 0.17  # 0.17 kg CO2 per km for cars (diesel)
+        return JsonResponse({"message": "Carbon footprint created"})
+
+        
