50df5827c97c1d64340a8b29e7cdd0911548a3af
dingbat32 <sven.willett@gmail.com>
2025-08-31 09:29:50 +1000

    current_train and choose_route

--- backend/code/api/serializers.py
+++ backend/code/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import TripInfo, CarbonFootprint, CrowdSourcedData
+from .models import TripInfo, CarbonFootprint, CrowdSourcedData, Train
 
 class TripInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = TripInfo
+        fields = '__all__'
+
+class TrainSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Train
         fields = '__all__'
