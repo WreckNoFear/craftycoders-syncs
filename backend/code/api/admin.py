2ea2ed6a4e8c26fe5d8d7b9208d7eeb3badca723
Anthony <anthony.nguyen6002@gmail.com>
2025-08-30 21:58:58 +1000

    convert django model instances to json format for api

--- backend/code/api/admin.py
+++ backend/code/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import TripInfo, Train, CarbonFootprint, CrowdsourcedData
+
+admin.site.register(TripInfo)
+admin.site.register(Train)
+admin.site.register(CarbonFootprint)
+admin.site.register(CrowdsourcedData)
