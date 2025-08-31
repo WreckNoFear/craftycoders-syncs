8859f0573cc0a5ad992243d2441b1b3f34533b99
dingbat32 <sven.willett@gmail.com>
2025-08-31 10:31:56 +1000

    Add urls

--- backend/code/api/urls.py
+++ backend/code/api/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("register/", views.register_user, name="register"),
     path('request-trips/', views.request_trips, name="request-trips"),
     path('token-auth/', obtain_auth_token),
-    path('request-locations/', views.request_locations, name="request-locations")
+    path('request-locations/', views.request_locations, name="request-locations"),
+    path('choose-route/', views.choose_route, name="choose-route"),
+    path('current-train/', views.current_train, name="current-train"),
 ]
